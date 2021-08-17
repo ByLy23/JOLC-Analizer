ad01bf7d61fb39d90776c504af59a20da4ea4c14
Byron <3018080310101@ingenieria.usac.edu.gt>
2021-08-17 02:45:50 -0600

    Inicio y Analizador Finalizado

--- src/pages/Analizador.js
+++ src/pages/Analizador.js
@@ -1,5 +1,48 @@
 import React from 'react';
-
+import '../css/analizador.css';
+import CodeMirror from '@uiw/react-codemirror';
+import 'codemirror/theme/monokai.css';
+import 'codemirror/keymap/sublime';
 export const Analizador = () => {
-  return <div>Analizador</div>;
+  const code = 'const a=0';
+  const salida = 'Output> asdasd';
+  return (
+    <div className="analizer-container">
+      <div className="analizer animate__animated animate__fadeInUp">
+        <div className="analizer-card">
+          <input type="submit" className="analizar" value="Analizar" />
+          <div className="editor">
+            <CodeMirror
+              value={code}
+              options={{
+                theme: 'monokai',
+                keyMap: 'sublime',
+                mode: 'julia',
+                viewportMargin: Infinity,
+                autofocus: false,
+              }}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="consola animate__animated animate__fadeInUp">
+        <div className="analizer-card">
+          <h1 className="con-text">Consola</h1>
+          <div className="console">
+            <CodeMirror
+              value={salida}
+              options={{
+                theme: 'monokai',
+                keyMap: 'sublime',
+                mode: 'julia',
+                viewportMargin: Infinity,
+                readOnly: true,
+                autofocus: false,
+              }}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
--- src/pages/Inicio.js
+++ src/pages/Inicio.js
@@ -1,5 +1,47 @@
 import React from 'react';
-
+import { FiGithub, FiMail, FiLinkedin } from 'react-icons/fi';
+import { IconContext } from 'react-icons';
+import '../css/inicio.css';
 export const Inicio = () => {
-  return <div>Inicio</div>;
+  return (
+    <div className="card animate__animated animate__fadeInDown">
+      <div className="card-header">
+        <p className="nombre">Byron Antonio Orellana Alburez</p>
+        <p className="carnet">201700733</p>
+      </div>
+      <IconContext.Provider value={{ size: '1em', className: 'react-icons' }}>
+        <div className="card-contact">
+          <ul>
+            <li>
+              <a
+                href="https://github.com/ByLy23?tab=repositories"
+                target="_blank"
+              >
+                <FiGithub />
+                https://github.com/ByLy23?tab=repositories
+              </a>
+            </li>
+            <li>
+              <a
+                href="mailto:3018080310101@ingenieria.usac.edu.gt?subject=Contacto%20de%20Trabajo"
+                target="_blank"
+              >
+                <FiMail />
+                3018080310101@ingenieria.usac.edu.gt
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/byronorellana-byly23/"
+                target="_blank"
+              >
+                <FiLinkedin />
+                https://www.linkedin.com/in/byronorellana-byly23/
+              </a>
+            </li>
+          </ul>
+        </div>
+      </IconContext.Provider>
+    </div>
+  );
 };
