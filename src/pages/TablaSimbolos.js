357593758e1b18bf76fa65e10738549afb617e4c
Byron <3018080310101@ingenieria.usac.edu.gt>
2021-08-21 01:05:20 -0600

    Frontend terminado

--- src/pages/TablaSimbolos.js
+++ src/pages/TablaSimbolos.js
@@ -1,5 +1,27 @@
 import React from 'react';
-
+import { tableData } from '../components/tableData';
+import '../css/tabla.css';
 export const TablaSimbolos = () => {
-  return <div>Simbolos</div>;
+  return (
+    <div className="table-container animate__animated animate__fadeInUp">
+      <table className="table">
+        <tr>
+          <th>Header1</th>
+          <th>Header2</th>
+          <th>Header3</th>
+          <th>Header4</th>
+        </tr>
+        {tableData.map(({ header1, header2, header3, header4 }) => {
+          return (
+            <tr>
+              <td>{header1}</td>
+              <td>{header2}</td>
+              <td>{header3}</td>
+              <td>{header4}</td>
+            </tr>
+          );
+        })}
+      </table>
+    </div>
+  );
 };
