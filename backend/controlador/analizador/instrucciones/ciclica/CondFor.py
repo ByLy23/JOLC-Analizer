b118c52cd756f524cd1a231147825c0bf1d26b78
Byron <3018080310101@ingenieria.usac.edu.gt>
2021-09-03 20:25:41 -0600

    condicion for terminada falta gramatica

---
+++ backend/controlador/analizador/instrucciones/ciclica/CondFor.py
@@ -0,0 +1,38 @@
+from controlador.analizador.excepciones.Error import Error
+from controlador.analizador.simbolos.TablaSimbolos import TablaSimbolos
+from controlador.analizador.simbolos.Tipo import TipoDato
+from controlador.analizador.abstracto.Instruccion import Instruccion
+
+
+class CondFor(Instruccion):
+    def __init__(self, declaSignacion, condicion, actualizacion, instrucciones, linea, columna):
+        super().__init__(TipoDato.ENTERO, linea, columna)
+        self.declaSignacion = declaSignacion
+        self.actualizacion = actualizacion
+        self.condicion = condicion
+        self.instrucciones = instrucciones
+
+    def interpretar(self, arbol, tablaSimbolo):
+        nuevaTabla = TablaSimbolos(tablaSimbolo)
+        nuevaTabla.setNombre('For')
+        declaAsig = self.declaSignacion.interpretar(arbol, nuevaTabla)
+        if isinstance(declaAsig, Error):
+            return declaAsig
+        val = self.condicion.interpretar(arbol, nuevaTabla)
+        if isinstance(val, Error):
+            return val
+        if self.condicion.tipo != TipoDato.BOOLEANO:
+            return Error("Error Semantico", "Debe ser booleano", self.linea, self.columna)
+        while self.condicion.interpretar(arbol, nuevaTabla):
+            otraTabla = TablaSimbolos(nuevaTabla)
+            otraTabla.setNombre('ForDentro')
+            for i in range(0, len(self.instrucciones)):
+                a = self.instrucciones[i].interpretar(arbol, otraTabla)
+                if isinstance(a, Error):
+                    arbol.getErrores().append(a)
+                    arbol.actualizaConsola(a.retornaError())
+                # if return, break, continue
+            valActualizacion = self.actualizacion.interpretar(
+                arbol, nuevaTabla)
+            if isinstance(valActualizacion, Error):
+                return valActualizacion
