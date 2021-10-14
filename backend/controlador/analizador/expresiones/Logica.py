6ea8feed27dda5592ed900bbfe9c413bd0095a3f
Byron <3018080310101@ingenieria.usac.edu.gt>
2021-10-13 18:50:52 -0600

    Logicas terminadas

--- backend/controlador/analizador/expresiones/Logica.py
+++ backend/controlador/analizador/expresiones/Logica.py
@@ -27,6 +27,105 @@ class Logica(Instruccion):
             nodo.agregarAST(self.cond2.getNodo())
         return nodo
 
+    def traducir(self, arbol, tablaSimbolo):
+        codigo = ""
+        izq = der = uno = None
+        if self.condExcep != None:
+            uno = self.condExcep.traducir(arbol, tablaSimbolo)
+            if isinstance(uno, Error):
+                return uno
+            if self.condExcep.tipo == TipoDato.NOTHING:
+                return Error("Error Semantico", "Variable con dato nulo no puede ser ejecutada", self.linea, self.columna)
+        else:
+            izq = self.cond1.traducir(arbol, tablaSimbolo)
+            if isinstance(izq, Error):
+                return izq
+            der = self.cond2.traducir(arbol, tablaSimbolo)
+            if isinstance(der, Error):
+                return der
+            if self.relacion == opLogico.AND:
+                self.tipo = TipoDato.BOOLEANO
+                temp = arbol.newTemp()
+                lTrue1 = arbol.newLabel()
+                lTrue2 = arbol.newLabel()
+                lFalse2 = arbol.newLabel()
+                codigo += izq["codigo"]
+                codigo += der["codigo"]
+                codigo += arbol.getCond2(izq["temporal"], "==", "1.0", lTrue1)
+                codigo += arbol.assigTemp1(temp["temporal"], "0.0")
+                codigo += arbol.goto(lFalse2)
+                codigo += arbol.getLabel(lTrue1)
+                codigo += arbol.getCond2(der["temporal"], "==", "1.0", lTrue2)
+                codigo += arbol.assigTemp1(temp["temporal"], "0.0")
+                codigo += arbol.goto(lFalse2)
+                codigo += arbol.getLabel(lTrue2)
+                codigo += arbol.assigTemp1(temp["temporal"], "1.0")
+                codigo += arbol.getLabel(lFalse2)
+                '''
+                if(a==1){goto Lv1}
+                t=0
+                goto Lf2
+                Lv1:
+                if (c ==1){goto Lv2}
+                t=0
+                goto Lf2
+                lv2:
+                t=1
+                Lf2:
+                '''
+
+            if self.cond1.tipo == TipoDato.NOTHING or self.cond2.tipo == TipoDato.NOTHING:
+                return Error("Error Semantico", "Variable con dato nulo no puede ser ejecutada", self.linea, self.columna)
+        self.tipo = TipoDato.BOOLEANO
+        if self.relacion == opLogico.OR:
+            temp = arbol.newTemp()
+            lTrue1 = arbol.newLabel()
+            lTrue2 = arbol.newLabel()
+            lFalse2 = arbol.newLabel()
+            codigo += izq["codigo"]
+            codigo += der["codigo"]
+            codigo += arbol.getCond2(izq["temporal"], "==", "1.0", lTrue2)
+            codigo += arbol.assigTemp1(temp["temporal"], "0.0")
+            codigo += arbol.goto(lTrue1)
+            codigo += arbol.getLabel(lTrue1)
+            codigo += arbol.getCond2(der["temporal"], "==", "1.0", lTrue2)
+            codigo += arbol.assigTemp1(temp["temporal"], "0.0")
+            codigo += arbol.goto(lFalse2)
+            codigo += arbol.getLabel(lTrue2)
+            codigo += arbol.assigTemp1(temp["temporal"], "1.0")
+            codigo += arbol.getLabel(lFalse2)
+            '''
+                if(a==1){goto Lv2}
+                t=0
+                goto Lv1
+                Lv1:
+                if (c ==1){goto Lv2}
+                t=0
+                goto Lf2
+                lv2:
+                t=1
+                Lf2:
+                '''
+        elif self.relacion == opLogico.NOT:
+            temp = arbol.newTemp()
+            lTrue1 = arbol.newLabel()
+            lFalse2 = arbol.newLabel()
+            codigo += uno["codigo"]
+            codigo += arbol.getCond2(uno["temporal"], "==", "1.0", lFalse2)
+            codigo += arbol.assigTemp1(temp["temporal"], "1.0")
+            codigo += arbol.goto(lTrue1)
+            codigo += arbol.getLabel(lFalse2)
+            codigo += arbol.assigTemp1(temp["temporal"], "0.0")
+            codigo += arbol.getLabel(lTrue1)
+            '''
+                if(a==1){goto Lf1}
+                t=1
+                goto Lv1
+                Lv1:
+                Lf1:
+                '''
+        return {'temporal': temp["temporal"], 'codigo': codigo}
+
     def interpretar(self, arbol, tablaSimbolo):
         izq = der = uno = None
         if self.condExcep != None:
